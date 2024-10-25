3ca4c0f9393b0ecbdf7ca5a2d8194a44a12d4f7d
ericc346 <ericc346@nycstudents.net>
2024-10-24 21:59:29 -0400

    Initial commit

---
+++ src/Lottery.java
@@ -0,0 +1,17 @@
+public class Lottery {
+    private int lotteryNumber;
+
+    /* constructor */
+    public Lottery(){
+        lotteryNumber = (int)(Math.random()*100);
+    }
+// initialize the lotteryNumber in the constructor to a random number
+
+    /* Returns 100, 10, 3, or 0, based on whether userNumber won anything */
+    public int determineWinnings(int userNumber){
+
+    }
+
+// ADD ANY OTHER METHOD(S) YOU THINK MIGHT BE USEFUL
+
+}
