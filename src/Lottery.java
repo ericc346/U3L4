757e8c0efce19c9eafa44d3beca0622af84e03fd
ericc346 <ericc346@nycstudents.net>
2024-10-24 23:07:22 -0400

    finsihed!!!

--- src/Lottery.java
+++ src/Lottery.java
@@ -1,17 +1,52 @@
 public class Lottery {
     private int lotteryNumber;
 
-    /* constructor */
+
     public Lottery(){
         lotteryNumber = (int)(Math.random()*100);
-    }
-// initialize the lotteryNumber in the constructor to a random number
 
-    /* Returns 100, 10, 3, or 0, based on whether userNumber won anything */
+    }
+
     public int determineWinnings(int userNumber){
+        if(userNumber == lotteryNumber){
+            return 100;
+        }
+        else if((userNumber%10) == (lotteryNumber/10) && (userNumber/10) == (lotteryNumber%10)){
+            return 10;
+        }
+        else if((userNumber%10) == (lotteryNumber/10)){
+            return 3;
+        }
+        else if((userNumber/10) == (lotteryNumber%10) && userNumber > 10){
+            return 3;
+        }
+        else if((userNumber%10) == (lotteryNumber%10)){
+            return 3;
+        }
+        else if((userNumber/10) == (lotteryNumber/10) && userNumber > 10){
+            return 3;
+        }
+        else{
+            return 0;
+        }
 
     }
-
-// ADD ANY OTHER METHOD(S) YOU THINK MIGHT BE USEFUL
+    public void displayLottery(){
+        System.out.println("The lottery number is " + lotteryNumber);
+    }
+    public void displayWinnings(int money){
+        if(money == 100){
+            System.out.println("Exact match:  You win $100!");
+        }
+        else if(money == 10){
+            System.out.println("Match all digits but not in correct order:  You win $10!");
+        }
+        else if(money == 3){
+            System.out.println("Match one digit: you win $3!");
+        }
+        else {
+            System.out.println("No matching numbers, you win nothing :(");
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        Lottery n = new Lottery();
-
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter your lottery pick: ");
+        int pick = Integer.parseInt(userInput.nextLine());
+        Lottery number = new Lottery();
+        number.displayLottery();
+        int pot = number.determineWinnings(pick);
+        number.displayWinnings(pot);
     }
 }
